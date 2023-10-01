edd7b30e707bca0ad18f455696fa1ba6755de799
Viettran1995 <144688233+Viettran1995@users.noreply.github.com>
2023-10-01 23:16:02 +0700

    Add files via upload

---
+++ js/typing-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+         strings: ["Photographer.", "Videographer.","Filmmaker.","Designer."],
+    typeSpeed:20,
+        backSpeed:20,
+        loop:true,
+      });
